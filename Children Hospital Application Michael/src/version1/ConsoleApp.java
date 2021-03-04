d59efcb9947514a8cb9034679ad55ea63d86e389
RilleMichi <78823904+RilleMichi@users.noreply.github.com>
2021-03-04 10:34:18 +0100

    commit

--- Children Hospital Application Michael/src/version1/ConsoleApp.java
+++ Children Hospital Application Michael/src/version1/ConsoleApp.java
@@ -5,28 +5,25 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import version1.Pediatrition.Title;
-import version1.Person.Gender;
-import version1.Vaccination.Vaccine;
 
 public class ConsoleApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Parent parent1 = new Parent("Huu", "Nguyen", Gender.MALE, "9876543210", "4537 Wiedlisbach");
-		Patient patient1 = new Patient ("Michael", "Nguyen", Gender.MALE, LocalDate.of(1998, 06, 17), parent1, Insurance.ASSURA);
+		Parent parent1 = new Parent("Huu", "Nguyen", Person.Gender.MALE, "9876543210", "4537 Wiedlisbach");
+		Patient patient1 = new Patient ("Michael", "Nguyen", Person.Gender.MALE, LocalDate.of(1998, 06, 17), parent1, Insurance.ASSURA);
 
-		Parent parent2 = new Parent("Sarah", "Lobsiger", Gender.FEMALE, "132456789", "3000 Bern");
-		Patient patient2 = new Patient("Lars", "Meyer", Gender.MALE, LocalDate.of(2010, 12, 31), parent2, Insurance.KPT);
+		Parent parent2 = new Parent("Sarah", "Lobsiger", Person.Gender.FEMALE, "132456789", "3000 Bern");
+		Patient patient2 = new Patient("Lars", "Meyer", Person.Gender.MALE, LocalDate.of(2010, 12, 31), parent2, Insurance.KPT);
 		
-		Pediatrition pediatrition1 = new Pediatrition("Adrian", "Casty", Gender.MALE, Title.Dr_Med);
-		Pediatrition pediatrition2 = new Pediatrition("Heinni", "Hans", Gender.MALE, Title.Prof_Dr);
+		Pediatrition pediatrition1 = new Pediatrition("Adrian", "Casty", Person.Gender.MALE, Pediatrition.Title.Dr_Med);
+		Pediatrition pediatrition2 = new Pediatrition("Heinni", "Hans", Person.Gender.MALE, Pediatrition.Title.Prof_Dr);
 		
 		Checkup checkup1 = new Checkup(70.3, 175, 36.5, false, pediatrition1);
 		Checkup checkup2 = new Checkup(85.2, 185, 37.5, true, LocalDate.of(2019, 12, 15), pediatrition2);
 		
-		Vaccination vaccination1 = new Vaccination(Vaccine.TETANUS, pediatrition1);
-		Vaccination vaccination2 = new Vaccination(Vaccine.COVID, pediatrition2);
+		Vaccination vaccination1 = new Vaccination(Vaccination.Vaccine.TETANUS, pediatrition1);
+		Vaccination vaccination2 = new Vaccination(Vaccination.Vaccine.COVID, pediatrition2);
 		
 		patient1.addCheckup(checkup1);
 		patient1.addCheckup(checkup2);
